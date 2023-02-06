432e79df8005ff1943cc9923d9a242f2108e74a2
Anandhan007 <98704896+Anandhan007@users.noreply.github.com>
2023-02-06 22:48:18 +0530

    Update lab3.py

--- lab3.py
+++ lab3.py
@@ -1,5 +1,5 @@
 #write a program that prints out all the elements of the list that are less than 5.
-# #Take this list
+#Take this list
 a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
 for x in a:
     if x<5:
